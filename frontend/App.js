42f3f7cfaab49e54df58165a39a70a9fe1df3102
Allan Paul Lopez <48453731+allanation@users.noreply.github.com>
2023-09-11 17:54:32 -0400

    set up frontend expo

---
+++ frontend/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NativeRouter, Route, Link } from "react-router-native";
+export default function App() {
+  return (
+    <NativeRouter>
+      <View style={styles.container}>
+        <Text>Hey bestie!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NativeRouter>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
